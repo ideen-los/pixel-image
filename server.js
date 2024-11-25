46768a1616928037f450ed0964e77b491fb3efa7
Robert Arning <r.arning@ub.uni-mainz.de>
2024-11-25 20:19:15 +0100

    ENH: extend rate limit

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ app.use(helmet());
 // Apply rate limiting to all requests
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // Limit each IP to 100 requests per windowMs
+  max: 1000, // Limit each IP to 100 requests per windowMs
   message: 'Too many requests from this IP, please try again after 15 minutes',
 });
 app.use(limiter);
